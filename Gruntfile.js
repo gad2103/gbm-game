a030db9810b688d33e430bae2cbbd2239bceec8f
gabriel duncan <gad2103@gmail.com>
2014-05-28 00:07:51 -0400

    started adding classes to tetris pieces instead of inline styles.

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = function (grunt) { 
+
+  // load grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+        // our Grunt task settings
+  });
+};
